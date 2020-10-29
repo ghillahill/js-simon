f6632348954928c567a8c1ad0f44fb72bfa8e8d1
Alberto <thebertsinfo@gmail.com>
2020-10-29 17:04:15 +0100

    Aggiunta funzione per prompt utente.

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
-    //Visualizzare in pagina 5 numeri casuali.
-    //Da lì parte un timer di 30 secondi.
-    //Dopo 30 secondi, vengono rimossi i numeri dalla pagina e l'utente deve inserire (tramite prompt) i numeri che ha visto precedentemente, uno alla volta.
+
+
+    //e l'utente deve inserire (tramite prompt) i numeri che ha visto precedentemente, uno alla volta.
     //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
     //BONUS: visualizzare in pagina anche un timer con il countdown dei 30 secondi
 
+    //Visualizzare in pagina 5 numeri casuali.
     var listaNumeriRandom = [];
 
     $( ".box-info p" ).each(function() {
@@ -21,22 +22,32 @@ $(document).ready(function () {
 
     $('.box-info p').append(listaNumeriRandom.join(' , '));
 
-    var timeLeft = 30;
+    //Da lì parte un timer di 30 secondi.
+    var tempoRimanente = 30;
     var displayTimer = $('.box-info h2');
 
     var timerId = setInterval(countdown, 1000);
 
     function countdown() {
-      if (timeLeft == -1) {
+      if (tempoRimanente == -1) {
         clearTimeout(timerId);
-        //getUserInput();
+        getUserInput();
       } else {
-        $('.box-info h2').text('Timer: ' + timeLeft + ' secondi rimanenti!');
-        timeLeft--;
+        $('.box-info h2').text('Timer: ' + tempoRimanente + ' secondi rimanenti!');
+        tempoRimanente--;
       }
     }
 
-    
+    //Dopo 30 secondi, vengono rimossi i numeri dalla pagina
+    function getUserInput() {
+
+        $( ".box-info p" ).toggleClass('active');
+        $('.box-info h2').toggleClass('active');
+
+        for (var i = 0; i < 5; i++) {
+            var sceltaUtente = parseInt(prompt("Digita uno dei numeri!"));
+        }
+    }
 
     function getRndInteger(min, max) {
       return Math.floor(Math.random() * (max - min + 1) ) + min;
